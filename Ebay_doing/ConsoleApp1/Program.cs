9ea4e128055857ee21d6cb60cb1e9cb72c598ac7
Denys "Den4ikLvivUA" Havryliak <35520974+Den4ikLvivUA@users.noreply.github.com>
2019-02-06 19:02:10 +0200

    0.2 V
    Rewrote code, also added catching of new exceptions by Assert method

--- Ebay_doing/ConsoleApp1/Program.cs
+++ Ebay_doing/ConsoleApp1/Program.cs
@@ -8,80 +8,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /*
-                                    Steps
-            ======
-                1. Go to https://www.ebay.com
-                2. Enter "Astronomical Telescope" into the search field
-                3. Click "Search" button
-                4. Get prices first 10 found items
-                5. Verify that sum of prices from the last steps is more then 1000$
-            */
-            string Price;
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.ebay.com/");
-            driver.FindElement(By.Id("gh-ac")).SendKeys("Astronomical Telescope");
-            driver.FindElement(By.Id("gh-btn")).Click();
-            Console.ReadKey();
-            Console.WriteLine(Finding10Prices(driver));
+            Console.WriteLine("Program Started!!!");
         }
-        public static double Finding10Prices(IWebDriver driver)
-        {
-            double Suma = 0;
-            String temp = "0";
-            /*for (int i = 0; i <= 10; i++)
-            {
-                try
-                {
-                    temp = driver.FindElement(By.CssSelector("span.bold")).Text;
-                    Console.WriteLine(temp);
-                }
-                catch
-                {
-                    Console.WriteLine("Элемент class: span.bold - не найдено");
-                    try
-                    {
-                        temp = driver.FindElement(By.CssSelector("span.s-item__price")).Text;
-                        Console.WriteLine(temp);
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Элемент class: span.s-item__price - не найдено еще раз!");
-                    }
-                    finally
-                    {
-                        temp.Replace('$', ' ');
-                        temp.Replace('.', ',');
-                        Console.WriteLine(temp);
-                        Suma += Convert.ToDouble(temp.Replace("$", " "));
-                        Console.WriteLine("Удалили первый элемент");
-                        Console.WriteLine(temp);
-                        //Suma += Convert.ToDouble(temp.Replace('.', ',');
-                    }
-                }
-                finally
-                {
-                    Suma += Convert.ToDouble(temp.Replace("$", " "));
-                }
-                else if 
-                {
-                    temp = driver.FindElement(By.ClassName("lvprice prc")).Text;
-                    Suma += double.Parse(temp);
-                    return Suma;
-                }
-                //lvprice prc
-                //s-item__price
-                //css=span.s-item__price
-                Console.WriteLine(Suma);
-            }*/
-
-
-            temp = driver.FindElement(By.XPath(".//*[@id='ListViewInner']//li/ul/li/span")).Text;
-            Console.WriteLine(temp);
-            Suma = (double)temp;
-            Console.WriteLine("FINALLY COUNT:");
-            Console.WriteLine(Suma);
-            return Suma;
-        }
+        
     }
 }
--- Ebay_doing/Ebay_doing/UnitTest1.cs
+++ Ebay_doing/Ebay_doing/UnitTest1.cs
@@ -1,4 +1,17 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+/*
+                                    Steps
+            ======
+                1. Go to https://www.ebay.com
+                2. Enter "Astronomical Telescope" into the search field
+                3. Click "Search" button
+                4. Get prices first 10 found items
+                5. Verify that sum of prices from the last steps is more then 1000$
+*/
 
 namespace Tests
 {
@@ -12,7 +25,72 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            string Price;
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ebay.com/");
+            driver.FindElement(By.Id("gh-ac")).SendKeys("Astronomical Telescope");
+            driver.FindElement(By.Id("gh-btn")).Click();
+            Console.ReadKey();
+            Console.WriteLine(Finding10Prices(driver));
             Assert.Pass();
+        }
+        public static double Finding10Prices(IWebDriver driver)
+        {
+            double Suma = 0;
+            String temp = "0";
+            /*for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    temp = driver.FindElement(By.CssSelector("span.bold")).Text;
+                    Console.WriteLine(temp);
+                }
+                catch
+                {
+                    Console.WriteLine("Ёлемент class: span.bold - не найдено");
+                    try
+                    {
+                        temp = driver.FindElement(By.CssSelector("span.s-item__price")).Text;
+                        Console.WriteLine(temp);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Ёлемент class: span.s-item__price - не найдено еще раз!");
+                    }
+                    finally
+                    {
+                        temp.Replace('$', ' ');
+                        temp.Replace('.', ',');
+                        Console.WriteLine(temp);
+                        Suma += Convert.ToDouble(temp.Replace("$", " "));
+                        Console.WriteLine("”далили первый элемент");
+                        Console.WriteLine(temp);
+                        //Suma += Convert.ToDouble(temp.Replace('.', ',');
+                    }
+                }
+                finally
+                {
+                    Suma += Convert.ToDouble(temp.Replace("$", " "));
+                }
+                else if 
+                {
+                    temp = driver.FindElement(By.ClassName("lvprice prc")).Text;
+                    Suma += double.Parse(temp);
+                    return Suma;
+                }
+                //lvprice prc
+                //s-item__price
+                //css=span.s-item__price
+                Console.WriteLine(Suma);
+            }*/
+
+            try { temp = driver.FindElement(By.XPath(".//*[@id='ListViewInner']//li/ul/li/span")).Text; }
+            catch { Assert.Fail("Exception thrown!!!!"); }
+            Console.WriteLine(temp);
+            Suma = Double.Parse(temp);
+            Console.WriteLine("FINALLY COUNT:");
+            Console.WriteLine(Suma);
+            return Suma;
         }
     }
 }
